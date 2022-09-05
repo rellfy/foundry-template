ddbbc3a9777fac5b975fbb45d3d45413cc92bc61
Lorenzo <lorenzo@nioel.com>
2022-09-06 02:40:05 +1000

    fix: always try to verify

--- deploy.sh
+++ deploy.sh
@@ -9,16 +9,12 @@ source .env
 FILE_NAME=$1.s.sol
 CONTRACT_NAME=$1
 echo Deploying with script: "$FILE_NAME"
-# Pass verification commands to variable, only use if not deploying locally.
-VERIFICATION_COMMANDS=
-if [[ $RPC_URL == *"localhost"* ]] || [[ $RPC_URL == *"127.0.0.1"* ]]; then
-  VERIFICATION_COMMANDS="--etherscan-api-key $ETHERSCAN_KEY --verify"
-fi
 # Run script to deploy and verify
 forge script \
   script/"$FILE_NAME":"$CONTRACT_NAME" \
   --rpc-url "$RPC_URL" \
   --private-key "$PRIVATE_KEY" \
   --broadcast \
+  --etherscan-api-key "$ETHERSCAN_KEY" \
+  --verify \
   -vvvv \
-  -- "$VERIFICATION_COMMANDS"
